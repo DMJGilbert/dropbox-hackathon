ac98cfe341ede3dd581d0a79c2965d074cbd72e2
Max Maybury <max@maxjmay.co.uk>
2015-01-23 21:15:05 +0000

    Welcome to the devbox!

---
+++ client/scripts/devbox.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	var editor = ace.edit("editor");
+	editor.setTheme("ace/theme/twilight");
+	var XmlMode = require("ace/mode/xml").Mode;
+	editor.getSession().setMode(new XmlMode());
+};
