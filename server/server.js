0e9bcb955d7dc1821b779da859f29732590256e3
Darren Gilbert <DMJGilbert@me.com>
2015-01-24 01:36:25 +0000

    Start of dropbox auth

--- server/server.js
+++ server/server.js
@@ -1,19 +1,74 @@
 var http = require('http');
 var path = require('path');
 var port = 80;
-express = require('express.io');
+
+var express = require('express.io'),
+    passport = require('passport'),
+    DropboxStrategy = require('passport-dropbox').Strategy;
+
+var DROPBOX_APP_KEY = "aaw7bo9qqrx3lpr"
+var DROPBOX_APP_SECRET = "c06uxmcxvhomzor";
+
 app = express().http().io();
 
-//Set up app
 app.set('port', process.env.PORT || port);
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser())
-app.use(express.session({secret: 'devbox'}))
+app.use(passport.initialize());
+app.use(express.session({secret: 'devbox'}));
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, '../client')));
 
-//Start server
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new DropboxStrategy({
+    consumerKey: DROPBOX_APP_KEY,
+    consumerSecret: DROPBOX_APP_SECRET,
+    callbackURL: "http://localhost/auth/dropbox/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', {
+    user: req.user
+  });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+app.get('/auth/dropbox',
+  passport.authenticate('dropbox'),
+  function(req, res){
+  });
+
+app.get('/auth/dropbox/callback',
+  passport.authenticate('dropbox', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log(req);
+    res.redirect('/index.html');
+  });
+
 app.listen(port);
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
