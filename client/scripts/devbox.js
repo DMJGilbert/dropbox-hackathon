498b3df713503b352f45c0a2c02874423bae0914
Max Maybury <max@maxjmay.co.uk>
2015-01-24 06:35:45 +0000

    DevBox amazing stuff!

--- client/scripts/devbox.js
+++ client/scripts/devbox.js
@@ -14,8 +14,19 @@ socket.on('projects:files', function(ret){
 	console.log(ret);
 
 	for ( var i = 0; i < ret.stat.contents.length; i += 1) {
-		$('#projectFolder').append('<div class="item"><i class="fa fa-folder-open"></i><div style="display:inline-block;" class="content"><div id="'+'test'+'" onclick="getPath(this);" class="header"> '+ret.arr[i]+'</div></div></div>');
+		if (ret.stat.contents[i]['is_dir']) {
+			$('#projectFolder').append('<div class="item"><i class="fa fa-folder-open"></i><div style="display:inline-block;" class="content"><div id="'+'test'+'" onclick="getPath(this);" class="header"> '+ret.arr[i]+'</div></div></div>');
+		} else {
+			$('#projectFolder').append('<div onclick="loadFile(\''+ret.stat.contents[i]['path']+'\')" class="item"><i class="fa fa-file-o"></i><div style="display:inline-block;" class="content"><div id="'+'test'+'" onclick="getPath(this);" class="header"> '+ret.arr[i]+'</div></div></div>');
+		}
 	}
+	
+});
+
+socket.on('read:file', function(ret){
+
+	console.log(ret);
+
 	
 });
 
@@ -39,6 +50,6 @@ socket.on('error:login', function(ret){
   window.location.href= "/index.html"
 });
 
-function loadFile() {
-
+function loadFile(filePath) {
+	socket.emit("read:file", {id: projectID, });
 }
