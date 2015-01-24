992bb3596bd4e11fca7dfaa0c7b437ae596ab41e
Max Maybury <max@maxjmay.co.uk>
2015-01-24 05:49:23 +0000

    Project create shit.

--- client/scripts/devbox.js
+++ client/scripts/devbox.js
@@ -1,3 +1,17 @@
+var socket = io.connect();
+
+socket.emit("files:list", {path: "Uni"});
+
+socket.on('files:list', function(ret){
+
+	for ( var i = 0; i < ret.stat.contents.length; i += 1) {
+		if (ret.stat.contents[i]['is_dir'] ) {
+			$('#projectFolder').append('<div class="item"><i class="fa fa-folder-open"></i><div style="display:inline-block;" class="content"><div id="'+'test'+'" onclick="getPath(this);" class="header"> '+ret.arr[i]+'</div></div></div>');
+		} 
+	}
+	
+});
+
 window.onload = function() {
 	var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/twilight");
@@ -17,3 +31,7 @@ var socket = io.connect();
 socket.on('error:login', function(ret){
   window.location.href= "/index.html"
 });
+
+function loadFile() {
+
+}
--- client/scripts/projects.js
+++ client/scripts/projects.js
@@ -1,7 +1,7 @@
 var socket = io.connect();
 
 socket.on('error:login', function(ret){
-  window.location.href= "/index.html"
+	window.location.href= "/index.html"
 });
 
 
@@ -9,30 +9,38 @@ socket.emit("files:list", {});
 
 socket.on('files:list', function(ret){
 
-	console.log(ret);
-
-	for ( var i = 0; i < ret.stat.contents.length; i += 1) {
-		if (ret.stat.contents[i]['is_dir'] ) {
-			$('#sharedFolders').append('<div class="item"><i class="fa fa-folder-open"></i><div style="display:inline-block;" class="content"><div id="'+'test'+'" onclick="getPath(this);" class="header"> '+ret.arr[i]+'</div></div></div>');
-		} 
+	if(ret.path) {
+		$('#'+ret.path.substring(1).replace('%20', '')).append('&nbsp;<div style="background-color: rgb(144, 229, 149);" class="circular ui mini icon button"><i onclick="createProject(\''+ret.path.substring(1)+'\', \''+ret.path+'\')" class="icon plus"></i></div>')
+	} else {
+		for ( var i = 0; i < ret.stat.contents.length; i += 1) {
+			if (ret.stat.contents[i]['is_dir'] ) {
+				$('#sharedFolders').append('<div class="item"><i class="fa fa-folder-open"></i><div style="display:inline-block;" class="content"><div style="cursor:pointer;" id="'+ret.stat.contents[i]['path'].substring(1).replace(/\s/g, '')+'" onclick="getPath(\''+encodeURI(ret.stat.contents[i]['path'])+'\')" class="header"> '+ret.arr[i]+'</div></div></div>');
+			} 
+		}
 	}
-	
 });
 
 
+socket.on('project:create', function(ret){
 
-function getPath(folder) {
+	console.log(ret);
 
-	socket.emit("projects:files", {path: "Uni"});
+	$('#projectFolders').append('<div class="item"><i class="fa fa-folder-open"></i><div style="display:inline-block;" class="content"><div id="'+ret.stat.contents[i]['path'].substring(1).replace(/\s/g, '')+'" onclick="gotoProject(\''+encodeURI(ret.stat.contents[i]['path'])+'\')" class="header"> '+ret.arr[i]+'</div></div></div>');
+			
+});
 
-	console.log(folder);
+
+function getPath(folderPath) {
+
+	socket.emit("files:list", {path: folderPath});
+
 }
 
+function gotoProject(projectName) {
 
-socket.on('projects:files', function(ret){
+}
 
-	console.log(ret);
+function createProject(projectName, projectPath) {
 
-	// $(folder).append('<div class="item"><i class="fa fa-folder-open"></i><div style="display:inline-block;" class="content"><div onclick="getPath(this);" class="header"> '+ret.arr[i]+'</div></div></div>');
-	
-});
+	socket.emit("project:create", {name:projectName, path:projectPath});
+}
