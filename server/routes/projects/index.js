32d8ae6b3d843813146c95feeeea56ea64a37fb3
Darren Gilbert <darren.gilbert@techex.co.uk>
2015-01-24 13:19:36 +0000

    IT WASNT ME!

--- server/routes/projects/index.js
+++ server/routes/projects/index.js
@@ -8,7 +8,7 @@ var projects = [];
 exports.list = function (req) {
 	if (req.session.user) {
 		var retProjects = [];
-		for (var i = 0; i < 0; i++) {
+		for (var i = 0; i < projects.length; i++) {
 			console.log(project[i]);
 			var project = projects[i];
 			if (project.users.indexOf(req.session.user.id) != -1) {
